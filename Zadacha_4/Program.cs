aca9546f7b18dff865ed1758b360e2fbc42c3d03
Vasilij U <vasya-1001@yandex.ru>
2023-04-08 19:09:49 +0500

    Zadacha 4 compledet

--- Zadacha_4/Program.cs
+++ Zadacha_4/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+//ввести два числа, вывести максимальное и минимальное число
+int a, b, c, count = 0, max=0;
+
+Console.Write("Введите первое число для сравнения: ");
+int.TryParse(Console.ReadLine(), out a);
+Console.Write("Введите второе число для сравнения: ");
+int.TryParse(Console.ReadLine(), out b);
+Console.Write("Введите третье число для сравнения: ");
+int.TryParse(Console.ReadLine(), out c);
+if ((a!=b) && (b!=c) && (a!=c) && (a>0) && (b>0) && (c>0))
+{
+    while (count <3)
+    {
+        if (a>max) max = a;
+        if (b>max) max = b;
+        if (c>max) max = c;
+        count++;
+    }
+    
+    Console.Write("Max: " + max);
+}
+else
+{
+    Console.WriteLine("Oops");
+}
