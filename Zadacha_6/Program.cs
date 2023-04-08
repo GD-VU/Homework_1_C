657523f1b76f802fbddc6dee2d0747a36db22dfd
Vasilij U <vasya-1001@yandex.ru>
2023-04-08 19:27:11 +0500

    Zadacha 6 completed

--- Zadacha_6/Program.cs
+++ Zadacha_6/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.Clear();
+
+//целочисленное деление. определение, является ли число четным
+int a, ostatok;
+
+Console.Write("Введите число для проверки четности: ");
+int.TryParse(Console.ReadLine(), out a);
+
+ostatok=a%2;
+   
+if (ostatok==0) 
+{
+    Console.WriteLine("Да, число четное");
+}
+else
+{
+    Console.WriteLine("Нет, число не четное");
+}
